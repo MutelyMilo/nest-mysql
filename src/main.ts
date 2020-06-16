f2893cd24dba60868dfe49ada608de64f6ef2888
Phoenix <mutelymilo@gmail.com>
2020-06-16 16:56:34 +0800

    Update

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './app.module';
 import configuration from './config/configuration';
 import { ValidationPipe } from '@nestjs/common';
 // 安全性
+import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors();
+  app.use(cookieParser())
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(configuration.port);
